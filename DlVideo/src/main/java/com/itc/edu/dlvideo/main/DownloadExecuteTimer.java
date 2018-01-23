4d6804a0bc7b7f04b3f773eaf69ff812574c5b17
phapnd <hanhtinhxanh.21@gmail.com>
2018-01-24 00:38:41 +0700

    [CR-005] - Modify file name video     format: video_[video_container.id].mp4     Modify close ket noi db sau khi thuc hien lenh

--- DlVideo/src/main/java/com/itc/edu/dlvideo/main/DownloadExecuteTimer.java
+++ DlVideo/src/main/java/com/itc/edu/dlvideo/main/DownloadExecuteTimer.java
@@ -22,6 +22,8 @@ import com.google.api.services.youtube.model.ResourceId;
 import com.google.api.services.youtube.model.SearchResult;
 import com.itc.edu.database.MySqlAccess;
 import com.itc.edu.dlvideo.util.Config;
+import static com.itc.edu.dlvideo.util.FileUtils.deleteFile;
+import static com.itc.edu.dlvideo.util.FileUtils.moveFile;
 import com.itc.edu.dlvideo.util.Utility;
 import static com.itc.edu.dlvideo.util.Utility.prefixOS;
 import static com.itc.edu.dlvideo.util.Utility.replaceBadChars;
@@ -40,6 +42,12 @@ import java.util.List;
 
 22-01-2018, [CR-004] phapnd
     Modify duong dan luu file video. Ten file viet lien, phan cach dau _
+
+23-01-2018, [CR-005] phapnd
+    Modify file name video
+    format: video_[video_container.id].mp4
+    Modify close ket noi db sau khi thuc hien lenh
+
  */
 public class DownloadExecuteTimer extends TimerTask {
 
@@ -52,6 +60,7 @@ public class DownloadExecuteTimer extends TimerTask {
     String videoFolderBase;
     private static final Logger logger = Logger.getLogger(DownloadExecuteTimer.class);
     private static String prefixOS = prefixOS();
+
     public DownloadExecuteTimer(String timerId, String cHomeId, String cMonitorId) {
         // TODO Auto-generated constructor stub
         this.timerId = timerId;
@@ -98,10 +107,24 @@ public class DownloadExecuteTimer extends TimerTask {
                             startTime = System.currentTimeMillis();
                             logger.info("Start Downloading:|VIDEOID=" + vId);
                             dowloadHandle.download(vId, path);
-                            logger.info("Download Success:|VIDEOID=" + vId + "|take time=" + (System.currentTimeMillis()-startTime));
+                            logger.info("Download Success:|VIDEOID=" + vId + "|take time=" + (System.currentTimeMillis() - startTime));
                             //Insert video info to data base
                             VideoWraper vWraper = getVideoInfor(singleVideo);
-                            saveVideoInfo(vWraper);
+                            Integer intVideoID = saveVideoInfo(vWraper);
+                            if (intVideoID >= 0) {
+                                String desFile = videoFolderBase + prefixOS + cHomeId + "-" + cMonitorId + prefixOS + "video_" + intVideoID.toString() + ".mp4";
+                                String tempFile = videoFolderBase + prefixOS + cHomeId + "-" + cMonitorId + prefixOS + replaceBadChars(singleVideo.getSnippet().getTitle()) + ".mp4";
+                                if (!moveFile(tempFile, desFile)) {
+                                    logger.error("Cannot rename|" + tempFile + " to " + desFile);
+                                    if (!deleteFile(tempFile)) {
+                                        logger.error("Error delete file " + tempFile);
+                                    }
+                                }
+                            }
+                            else {
+                                logger.error("ERR_FILENAME_VIDEO|" + vWraper.monitorChannelId +"|" + vWraper.title);
+                                VideoContainerStatus(vWraper.title,intVideoID,100);
+                            }
                             //Notify to upload thread
                             //new UploadExecuteThread().getInstance().addElement(vWraper);	
                         }
@@ -124,7 +147,7 @@ public class DownloadExecuteTimer extends TimerTask {
         Date lastSyncTime = null;
         String monitorChannelId = null;
         String query = "SELECT LastSyncTime, monitorchannelid FROM home_monitor_channel_mapping WHERE Id = '" + mapId + "';";
-        Statement stmt;
+        Statement stmt = null;
         try {
             stmt = MySqlAccess.getInstance().connect.createStatement();
             ResultSet rs = stmt.executeQuery(query);
@@ -134,7 +157,18 @@ public class DownloadExecuteTimer extends TimerTask {
             }
         } catch (Exception ex) {
             logger.info(ex.toString());
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException ex) {
+                    // TODO Auto-generated catch block
+                    logger.error("ERR_GET_MontiorChannelID|" + ex.getMessage());
+                    return lastSyncTime;
+                }
+            }
         }
+
         logger.info("Get info: MontiorChannelID=" + monitorChannelId + "|LastSyncTime=" + lastSyncTime + "|take time:" + (System.currentTimeMillis() - startTime));
         return lastSyncTime;
     }
@@ -154,29 +188,39 @@ public class DownloadExecuteTimer extends TimerTask {
             // TODO Auto-generated catch block
             logger.info("ERR_UPDATE_LASTSYNCTIME|" + ex.getMessage());
             return;
+        } finally {
+            if (preparedStm != null) {
+                try {
+                    preparedStm.close();
+                } catch (SQLException ex) {
+                    // TODO Auto-generated catch block
+                    logger.error("ERR_UPDATE_LASTSYNCTIME|" + ex.getMessage());
+                    return;
+                }
+            }
         }
         logger.info("Update info: MontiorChannelID=" + timerId + "|LastSyncTime=" + lastSyncTime + "|take time:" + (System.currentTimeMillis() - startTime));
     }
 
     private VideoWraper getVideoInfor(SearchResult singleVideo) {
-        String temp;
         VideoWraper vWraper = new VideoWraper();
         vWraper.vId = singleVideo.getId().getVideoId();
         vWraper.title = singleVideo.getSnippet().getTitle();
         vWraper.title = vWraper.title.replaceAll("[!@#$%^&*(){}:|<>?]", " ");
-        temp = replaceBadChars(vWraper.title);
         vWraper.description = singleVideo.getSnippet().getDescription();
         vWraper.tag = singleVideo.getEtag();
         vWraper.thumbnail = singleVideo.getSnippet().getThumbnails().getDefault().getUrl();
-        vWraper.vLocation = videoFolderBase + prefixOS + cHomeId + "-" + cMonitorId + prefixOS + temp + ".mp4";
+        vWraper.vLocation = videoFolderBase + prefixOS + cHomeId + "-" + cMonitorId + prefixOS;
         return vWraper;
     }
 
-    private void saveVideoInfo(VideoWraper vWraper) {
+    private Integer saveVideoInfo(VideoWraper vWraper) {
+        Integer autoIncKeyFromFunc = -1;
         Long startTime = System.currentTimeMillis();
         Timestamp timestamp = new Timestamp(new Date().getTime());
         //insert to database
         PreparedStatement preparedStm = null;
+        ResultSet rs = null;
 //        String query = "INSERT INTO video_container (VideoId, Title, Tag, Description, Thumbnail, "
 //                + "VideoLocation, HomeChannelId, MonitorChannelId, DownloadDate) VALUES (?,?,?,?,?,?,?,?,?)";
         String query = "{ call INSERTNEWMO(?,?,?,?,?,?,?,?,?) }";
@@ -194,12 +238,76 @@ public class DownloadExecuteTimer extends TimerTask {
             preparedStm.setTimestamp(9, timestamp);
             preparedStm.executeUpdate();
             logger.info("Saved video " + vWraper.vId + " to database");
+
+//
+            // Use the MySQL LAST_INSERT_ID()
+            // function to do the same thing as getGeneratedKeys()
+            //
+            rs = preparedStm.executeQuery("SELECT LAST_INSERT_ID()");
+
+            if (rs.next()) {
+                autoIncKeyFromFunc = rs.getInt(1);
+                logger.info("Last sequence for Indexing VideoID=" + autoIncKeyFromFunc);
+            } else {
+                // TODO Auto-generated catch block
+                logger.error("ERR_LAST_SEQ_VIDEOCONTAINER|" + autoIncKeyFromFunc);
+                return autoIncKeyFromFunc;
+            }
+
         } catch (SQLException e) {
             // TODO Auto-generated catch block
             logger.error("ERR_SAVE_VIDEOCONTAINER|" + e.getMessage());
-            return;
+            return autoIncKeyFromFunc;
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                    // TODO Auto-generated catch block
+                    logger.error("ERR_SAVE_VIDEOCONTAINER|" + ex.getMessage());
+                    return autoIncKeyFromFunc;
+                }
+            }
+            if (preparedStm != null) {
+                try {
+                    preparedStm.close();
+                } catch (SQLException ex) {
+                    // TODO Auto-generated catch block
+                    logger.error("ERR_SAVE_VIDEOCONTAINER|" + ex.getMessage());
+                    return autoIncKeyFromFunc;
+                }
+            }
         }
         logger.info("Insert info: MontiorChannelID=" + cMonitorId + "|VideoID=" + vWraper.vId + "|TITLE:" + vWraper.title + "|take time:" + (System.currentTimeMillis() - startTime));
+
+        return autoIncKeyFromFunc;
+    }
+
+    private void VideoContainerStatus(String title, int recordId, int status) {
+        String query = "{ call UPLOADSTATUS(?,?) }";
+        PreparedStatement preparedStm = null;
+        try {
+            preparedStm = MySqlAccess.getInstance().connect.prepareStatement(query);
+            preparedStm.setInt(1, recordId);
+            preparedStm.setInt(2, status);
+            preparedStm.executeUpdate();
+            logger.info("Update VIDEOCONTAINER STATUS: Video= " + title + "|STATUS=" + status);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            logger.error("ERR_UPDATE_VIDEOCONTAINER_STATUS|" + e.getMessage());
+            return;
+        } finally {
+            if (preparedStm != null) {
+                try {
+                    preparedStm.close();
+                } catch (SQLException ex) {
+                    // TODO Auto-generated catch block
+                    logger.error("ERR_UPDATE_VIDEOCONTAINER_STATUS|" + ex.getMessage());
+                    return;
+                }
+            }
+        }
+        //logger.info("Insert info: MontiorChannelID=" + cMonitorId + "|VideoID=" + vWraper.vId + "|TITLE:" + vWraper.title + "|take time:" + (System.currentTimeMillis() - startTime));
 
     }
 
--- DlVideo/src/main/java/com/itc/edu/dlvideo/main/DownloadTimerManager.java
+++ DlVideo/src/main/java/com/itc/edu/dlvideo/main/DownloadTimerManager.java
@@ -1,4 +1,5 @@
 package com.itc.edu.dlvideo.main;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -12,58 +13,72 @@ import com.itc.edu.timer.TimerWrapper;
 
 /*------------------------------------------------------------------------------
 ** History
+23-01-2018, [CR-005] phapnd
+    Modify close ket noi db sau khi thuc hien lenh
 
-*/
+ */
+public class DownloadTimerManager extends TimerWrapper {
 
-public class DownloadTimerManager extends TimerWrapper{
-	private static DownloadTimerManager instance = null;
-	private static Logger logger = Logger.getLogger(DownloadTimerManager.class);
-	
-	public DownloadTimerManager() {
-	}
-	
-	public static DownloadTimerManager getInstance(){
-		if(instance == null){
-			instance = new DownloadTimerManager();
-		}
-		return instance;
-	}
-	
-	public boolean startDownloadTimer(String taskId, String cHomeId, String cMonitorId, int timerInterval) {
-		boolean isSuccess = false;
-		TimerTask timerTask = new DownloadExecuteTimer(taskId, cHomeId, cMonitorId);
-		Timer timer = new Timer();
-		timer.scheduleAtFixedRate(timerTask, 0, timerInterval);
-		if(timer!= null){
-			timerMap.put(taskId, timer);	
-		}
-		isSuccess = true;
-		return isSuccess;
-	}
-	
-        @Override
-	public void initTimerTask(){
-		
-		long startTime = 0L;
-		
-		logger.info("init Get MonitorChannel");
-		Statement stmt;
-		String query = "SELECT Id, HomeChannelId, MonitorChannelId, TimeIntervalSync, StatusSync "
-				+ "FROM home_monitor_channel_mapping WHERE StatusSync = '1';";
-		try {
-			stmt = MySqlAccess.getInstance().connect.createStatement();
-			ResultSet rs = stmt.executeQuery(query);
-			while (rs.next()) {
-				String id = Integer.toString(rs.getInt(1));
-				String cHomeId = rs.getString(2);
-				String cMonitorId = rs.getString(3);
-				int syncInterval = rs.getInt(4);
-				startDownloadTimer(id, cHomeId, cMonitorId, syncInterval * 1000);
-				logger.info("Start sync task : " + id + " with timer interval = " + 1 + " seconds");
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			logger.error("ERR_GET_MONITORCHANNEL|" + e);
-		}	
-	}
+    private static DownloadTimerManager instance = null;
+    private static Logger logger = Logger.getLogger(DownloadTimerManager.class);
+
+    public DownloadTimerManager() {
+    }
+
+    public static DownloadTimerManager getInstance() {
+        if (instance == null) {
+            instance = new DownloadTimerManager();
+        }
+        return instance;
+    }
+
+    public boolean startDownloadTimer(String taskId, String cHomeId, String cMonitorId, int timerInterval) {
+        boolean isSuccess = false;
+        TimerTask timerTask = new DownloadExecuteTimer(taskId, cHomeId, cMonitorId);
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(timerTask, 0, timerInterval);
+        if (timer != null) {
+            timerMap.put(taskId, timer);
+        }
+        isSuccess = true;
+        return isSuccess;
+    }
+
+    @Override
+    public void initTimerTask() {
+
+        long startTime = 0L;
+
+        logger.info("init Get MonitorChannel");
+        Statement stmt = null;
+        String query = "SELECT Id, HomeChannelId, MonitorChannelId, TimeIntervalSync, StatusSync "
+                + "FROM home_monitor_channel_mapping WHERE StatusSync = '1';";
+        try {
+            stmt = MySqlAccess.getInstance().connect.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                String id = Integer.toString(rs.getInt(1));
+                String cHomeId = rs.getString(2);
+                String cMonitorId = rs.getString(3);
+                int syncInterval = rs.getInt(4);
+                startDownloadTimer(id, cHomeId, cMonitorId, syncInterval * 1000);
+                logger.info("Start sync task : " + id + " with timer interval = " + 1 + " seconds");
+            }
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            logger.error("ERR_GET_MONITORCHANNEL|" + e);
+            return;
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException ex) {
+                    // TODO Auto-generated catch block
+                    logger.error("ERR_GET_MONITORCHANNEL|" + ex.getMessage());
+                    return;
+                }
+            }
+        }
+
+    }
 }
