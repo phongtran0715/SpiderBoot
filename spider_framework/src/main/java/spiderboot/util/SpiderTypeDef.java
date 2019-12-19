d79e0c4e913f5a83ad2a11816b1844f7aebe88c6
phongtran0715 <phongtran0715@gmail.com>
2019-12-19 17:20:59 +0700

    Remove spider_framewok project

--- spider_framework/src/main/java/spiderboot/util/SpiderTypeDef.java
+++
@@ -1,7 +0,0 @@
-package spiderboot.util;
-
-public class SpiderTypeDef {
-	public static final int DOWNLOADED 	= 0; 
-	public static final int RENDERED 	= 1; 
-	public static final int UPLOADED 	= 2; 
-}
